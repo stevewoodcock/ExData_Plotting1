374b616fc33701c414d5b45c641ae2cc4d21f812
Steve Woodcock <swoodcock@scholastic.co.uk>
2014-10-10 22:01:28 +0100

    plot1

---
+++ plot1.R
@@ -0,0 +1,18 @@
+library(dplyr)
+
+epc <-
+  readLines(pipe("egrep '^(Date|[12]/2/2007)' household_power_consumption.txt")) %>%
+  textConnection() %>%
+  read.csv(header=T, sep=";", na.strings="?") %>%
+  tbl_df %>%
+  mutate(
+    datetimechar = paste(as.character(Date), as.character(Time)),
+    datetime = as.POSIXct(strptime(datetimechar, "%d/%m/%Y %H:%M:%S")))
+
+png('plot1.png')
+hist(epc$Global_active_power,
+     col="red",
+     xlab="Global Active Power (kilowatts)",
+     ylab="Frequency",
+     main="Global Active Power")
+dev.off()
